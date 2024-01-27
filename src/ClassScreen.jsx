05d2c1970c6367fd2d0e6129bc61f07e195ada16
maxvysh <45989000+maxvysh@users.noreply.github.com>
2024-01-26 22:00:57 -0500

    Subject selector wip

--- src/ClassScreen.jsx
+++ src/ClassScreen.jsx
@@ -1,15 +1,21 @@
 import Header from "./components/Header";
 import ScreenSelector from "./components/ScreenSelector";
 import SelectedCourses from "./components/SelectedCourses";
+import SubjectSelector from "./components/SubjectSelector";
 
 const ClassScreen = () => {
   return (
     <div>
       <Header />
-      <body className="p-3">
+      <body className="p-3 flex">
         <div className="w-96">
           <ScreenSelector />
           <SelectedCourses />
+        </div>
+        <div>
+          <div>
+            <SubjectSelector />
+          </div>
         </div>
       </body>
     </div>
