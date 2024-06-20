533d613618fa7cb2123849c43cf43259962294cb
maxvysh <45989000+maxvysh@users.noreply.github.com>
2024-06-20 18:20:04 -0400

    chore: Refactor SectionScreen and ScreenSelector components

--- src/SectionScreen.jsx
+++ src/SectionScreen.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import Header from "./components/Header";
+import ScreenSelector from "./components/ScreenSelector";
+import SelectedCourses from "./components/SelectedCourses";
 
 const SectionScreen = () => {
   return (
-    <div>SectionScreen</div>
-  )
-}
+    <div>
+      <div className="min-w-[1570px]">
+        <Header />
+      </div>
+      <div className="p-3 flex">
+        <div className="w-[330px] min-w-[330px]">
+          <ScreenSelector />
+          <div className="mt-4">
+            {/* <SelectedCourses
+            //   selectedCourses={selectedCourses}
+            //   setSelectedCourses={setSelectedCourses}
+            //   totalCredits={totalCredits}
+            //   setTotalCredits={setTotalCredits}
+            //   buttonDisabler={buttonDisabler}
+            //   setButtonDisabler={setButtonDisabler}
+            /> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SectionScreen
+export default SectionScreen;
--- src/components/ScreenSelector.jsx
+++ src/components/ScreenSelector.jsx
@@ -1,13 +1,64 @@
 import { Card } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const ScreenSelector = () => {
+  const location = useLocation();
+  const currentPage = location.pathname;
+  const navigate = useNavigate();
+
+  const handleClick = (page) => {
+    // fetch(`http://localhost:3000/${page}`)
+    //   .then((response) => response.text())
+    //   .then((data) => {
+    //     console.log(data);
+    //   })
+    //   .catch((error) => console.error("Error:", error));
+
+    // return () => {
+    //   window.location.href = `http://localhost:3000/${page}`;
+    // };
+
+    return () => navigate(`/${page}`);
+  };
+
   return (
     <Card className="flex flex-col justify-evenly h-52 p-2 border-2">
-      <Button className="text-base">Select Schedule</Button>
-      <Button variant="outline" className="border-2 text-base text-gray-500">Select Sections</Button>
-      <Button variant="outline" className="border-2 text-base text-gray-500">Build Schedule</Button>
-      <Button variant="outline" className="border-2 text-base text-gray-500">Saved Schedule</Button>
+      {currentPage === "/classes" ? (
+        <Button className="text-base">Select Schedule</Button>
+      ) : (
+        <Button variant="outline" className="border-2 text-base text-gray-500" onClick={handleClick('classes')}>
+          Select Schedule
+        </Button>
+      )}
+
+      {currentPage === "/sections" ? (
+        <Button className="text-base">Select Sections</Button>
+      ) : (
+        <Button variant="outline" className="border-2 text-base text-gray-500" onClick={handleClick('sections')}>
+          Select Sections
+        </Button>
+      )}
+
+      {currentPage === "/build-schedule" ? (
+        <Button className="text-base">Build Schedule</Button>
+      ) : (
+        <Button variant="outline" className="border-2 text-base text-gray-500">
+          Build Schedule
+        </Button>
+      )}
+
+      {currentPage === "/saved-schedules" ? (
+        <Button className="text-base">Saved Schedules</Button>
+      ) : (
+        <Button variant="outline" className="border-2 text-base text-gray-500">
+          Saved Schedules
+        </Button>
+      )}
+
+      {/* <Button variant="outline" className="border-2 text-base text-gray-500">Select Sections</Button>
+        <Button variant="outline" className="border-2 text-base text-gray-500">Build Schedule</Button>
+        <Button variant="outline" className="border-2 text-base text-gray-500">Saved Schedule</Button> */}
     </Card>
   );
 };
