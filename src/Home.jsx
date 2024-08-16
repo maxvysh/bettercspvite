1a652867ea195a69652ddac582fc1346c832b067
maxvysh <maxvysh@gmail.com>
2024-08-16 00:46:23 -0400

    home page looks good

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,51 +2,42 @@ import { useEffect, useRef } from "react";
 import CreateAccount from "@/components/CreateAccount";
 import { Button } from "@/components/ui/button";
 import "./css/Home.css";
+import cloudframe from "./assets/cloud_frame.svg";
 
 function Home() {
-  const topTextRef = useRef(null);
-  const bottomTextRef = useRef(null);
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (bottomTextRef.current && topTextRef.current) {
-        const bottomTextWidth = bottomTextRef.current.offsetWidth;
-        topTextRef.current.style.width = `${bottomTextWidth}px`;
-      }
-    };
-
-    window.addEventListener("resize", handleResize);
-    handleResize(); // Initial call to set the width
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
   return (
-    <div className="h-screen bg-gradient-to-b from-[#0f172a] via-[#305180] via-[#3361a1] to-[#f8fafc]">
-      <div className="flex flex-col">
-        <div className="animated-title ml-8 h-fit -mt-6">
-          <div className="text-top" ref={topTextRef}>
+    <div
+      className="h-screen flex flex-col min-w-[340px]"
+      style={{
+        backgroundImage: `url(${cloudframe})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="pt-8 flex justify-center">
+        <div className="animated-title h-fit text-center flex flex-col items-center">
+          <div className="text-top">
             <div>
-              <span className="text-[172px] text-white -mt-10">RUPlanning</span>
+              <span className="text-6xl sm:text-7xl md:text-8xl lg:text-9xl xl:text-[172px] text-white">
+                RUPlanning
+              </span>
             </div>
           </div>
-          <div className="text-bottom" ref={bottomTextRef}>
+          <div className="text-bottom">
             <div>
-              <span className="text-[72px] text-white">
+              <span className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-white">
                 A better way to plan your Rutgers schedule
               </span>
             </div>
           </div>
         </div>
-        <div className="flex flex-col items-center justify-center gap-6">
-          <CreateAccount />
-          <div className="flex flex-col max-w-[426px] w-full gap-2">
-            <Button className="bg-white text-[#020817] hover:bg-[#cbd5e1]">
-              Learn More About RUPlanning
-            </Button>
-          </div>
+      </div>
+      <div className="flex-grow flex flex-col items-center justify-center gap-6 mx-2 mb-60">
+        <CreateAccount />
+        <div className="flex flex-col max-w-[426px] w-full gap-2">
+          <Button className="bg-white text-[#020817] hover:bg-[#cbd5e1]">
+            Learn More About RUPlanning
+          </Button>
         </div>
       </div>
     </div>
